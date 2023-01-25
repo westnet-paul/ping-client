953ae90205544e5412801bbb29d0199ef4b2123d
Paul Cunnane <paul.cunnane@westnet.ie>
2023-01-25 15:45:33 +0000

    Test refactor

--- tests/test_ping.py
+++ tests/test_ping.py
@@ -3,6 +3,9 @@ from ping import Ping, PingError
 import pytest
 
 
+TEST_URL = "http://py.test"
+
+
 def test_ping_noenv():
 
     with pytest.raises(PingError):
@@ -11,7 +14,7 @@ def test_ping_noenv():
 
 def test_ping_env():
 
-    os.environ["PING_SERVER_URL"] = "http://py.test"
+    os.environ["PING_SERVER_URL"] = TEST_URL
     instance = Ping("1.1.1.1")
 
     assert instance
@@ -22,10 +25,10 @@ def test_alive(requests_mock):
     address = "1.2.3.4"
 
     requests_mock.get(
-        f"http://py.test/{address}",
+        f"{TEST_URL}/{address}",
         json={"address": address, "alive": True, "rtt": 1.23},
     )
-    os.environ["PING_SERVER_URL"] = "http://py.test"
+    os.environ["PING_SERVER_URL"] = TEST_URL
     instance = Ping(address)
 
     assert instance.address == address
@@ -38,10 +41,10 @@ def test_dead(requests_mock):
     address = "9.8.7.6"
 
     requests_mock.get(
-        f"http://py.test/{address}",
+        f"{TEST_URL}/{address}",
         json={"address": address, "alive": False},
     )
-    os.environ["PING_SERVER_URL"] = "http://py.test"
+    os.environ["PING_SERVER_URL"] = TEST_URL
     instance = Ping(address)
 
     assert instance.address == address
@@ -53,11 +56,11 @@ def test_invalid(requests_mock):
     address = "not.a.domain"
 
     requests_mock.get(
-        f"http://py.test/{address}",
+        f"{TEST_URL}/{address}",
         status_code=404,
         json={"detail": f"The name '{address}' cannot be resolved"},
     )
-    os.environ["PING_SERVER_URL"] = "http://py.test"
+    os.environ["PING_SERVER_URL"] = TEST_URL
     instance = Ping(address)
 
     with pytest.raises(PingError) as excinfo:
